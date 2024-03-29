b68c00a4b9f73a679c2eb8ec9c2e4c0e38d69399
José Mokeni <jmmokeni@gmail.com>
2024-03-30 00:19:56 +0100

    delete task by id, btw get task by id

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -24,10 +24,41 @@ router.get("/tasks", async (req, res) => {
   }
 });
 
+// Get a task by id
+router.get("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  try {
+    const task = await Task.findById(id);
+    console.log(task);
+    if (task === null) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+    res.status(200).json(task);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error.message });
+  }
+});
+
 // Delete all tasks
 router.delete("/tasks", async (req, res) => {
   try {
     await Task.deleteMany();
+    res.status(204).end();
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Delete a task by id
+router.delete("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const task = await Task.findByIdAndDelete(id);
+    if (!task) {
+      return res.status(404).json({ error: "Task not found" });
+    }
     res.status(204).end();
   } catch (error) {
     res.status(400).json({ error: error.message });
--- backend/test/tasks.test.js
+++ backend/test/tasks.test.js
@@ -59,3 +59,122 @@ describe("GET /tasks", () => {
       });
   });
 });
+
+describe("GET /tasks/:id", () => {
+  it("responds with json", async () => {
+    // add a task to the database
+    const response = await request(app)
+      .post("/tasks")
+      .send({
+        title: "Test Task",
+      })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(201)
+      .then((response) => {
+        const taskId = response.body._id;
+        return taskId;
+      });
+
+    // get the task by id
+    const response2 = await request(app)
+      .get(`/tasks/${response}`)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            title: "Test Task",
+          })
+        );
+      });
+  });
+
+  it("responds with 400 if the id is invalid", async () => {
+    // get a task by an invalid id
+    const response = await request(app)
+      .get(`/tasks/aaaa`)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(400)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            error: expect.any(String),
+          })
+        );
+      });
+  });
+
+  it("responds with 404 if the task is not found", async () => {
+    // get a task by an id that does not exist
+    const response = await request(app)
+      .get(`/tasks/60e7c9b7d3f5e9f8b8b4e9e1`)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(404)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            error: "Task not found",
+          })
+        );
+      });
+  });
+});
+
+// Test the DELETE route
+
+describe("DELETE /tasks", () => {
+  it("responds with 204", async () => {
+    const response = await request(app).delete("/tasks").expect(204);
+
+    // check if the database is empty
+    const response2 = await request(app)
+      .get("/tasks")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual([]);
+      });
+  });
+});
+
+describe("DELETE /tasks/:id", () => {
+  it("responds with 204", async () => {
+    // add a task to the database
+    const response = await request(app)
+      .post("/tasks")
+      .send({
+        title: "Test Task",
+      })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(201)
+      .then((response) => {
+        const taskId = response.body._id;
+        return taskId;
+      });
+
+    // delete the task by id
+    const response2 = await request(app)
+      .delete(`/tasks/${response}`)
+      .expect(204);
+
+    // check if the task is deleted
+    const response3 = await request(app)
+      .get(`/tasks/${response}`)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(404)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            error: "Task not found",
+          })
+        );
+      });
+  });
+});
