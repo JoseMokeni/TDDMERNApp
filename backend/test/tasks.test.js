7eeeedffbfa648b060ab23c44f2ca753ee7aa9b7
José Mokeni <jmmokeni@gmail.com>
2024-03-29 23:28:23 +0100

    test add task written

--- backend/test/tasks.test.js
+++ backend/test/tasks.test.js
@@ -1,1 +1,20 @@
-const request = require('supertest');
+const request = require("supertest");
+
+const app = require("../server");
+
+// Test the POST route
+
+describe("POST /tasks", () => {
+  it("responds with json", async () => {
+    const response = await request(app)
+      .post("/tasks")
+      .send({
+        title: "Test Task",
+      })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(201);
+
+    expect(response.body).toHaveProperty("title").equal("Test Task");
+  });
+});
